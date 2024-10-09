6065ed3d03977291d6c5da1cc77298e625700be9
Troy Alford <TroyAlford@users.noreply.github.com>
2024-10-08 20:48:54 -0700

    fix missing exports/registrations

--- libraries/react/testing/bun/index.ts
+++ libraries/react/testing/bun/index.ts
@@ -1,7 +1,9 @@
 import { afterAll, describe, expect, it, test } from 'bun:test'
 import { toHaveAttribute } from './matchers/toHaveAttribute'
 import { toHaveClass } from './matchers/toHaveClass'
+import { toHaveStyle } from './matchers/toHaveStyle'
 import { toHaveTextContent } from './matchers/toHaveTextContent'
+
 global.afterAll = afterAll
 global.describe = describe
 global.it = it
@@ -12,5 +14,6 @@ import './extensions/console'
 expect.extend({
   toHaveAttribute,
   toHaveClass,
+  toHaveStyle,
   toHaveTextContent,
 })
