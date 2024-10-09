da86841e1a1cc7cb08289a54e1f6d4830fb9e310
Troy Alford <TroyAlford@users.noreply.github.com>
2024-10-08 20:51:39 -0700

    fix(bun matchers): fix missing exports/registrations (#21)

--- libraries/react/testing/bun/index.d.ts
+++ libraries/react/testing/bun/index.d.ts
@@ -2,6 +2,7 @@ declare module 'bun:test' {
   interface Matchers {
     toHaveAttribute(name: string, value: string | RegExp): void,
     toHaveClass(...classes: string[]): void,
+    toHaveStyle(style: string | RegExp | Partial<CSSStyleDeclaration>): void,
     toHaveTextContent(text: string | RegExp): void,
   }
 }
